6d81bcf53a0b39f1468d1da594a3d36402911ad0
Jardeson Santos <jardounifanor@gmail.com>
2021-01-07 07:40:32 -0300

    "Atividade finalizada para possíveis correções."

--- features/steps/glossary_steps.py
+++ features/steps/glossary_steps.py
@@ -28,6 +28,5 @@ def glossary_menu(context):
 @then(u'the platform returns to the user the glossary items in alphabetical order')
 def glossary_items(context):
     glossary_items = context.browser.find_element_by_tag_name("h4")
-    glossary_items.text()
     assert_equal("Itens do Glossário ", glossary_items)
     context.browser.quit()
--- features/steps/my_signs_steps.py
+++ features/steps/my_signs_steps.py
@@ -28,6 +28,5 @@ def my_signs_menu(context):
 @then(u'the platform returns to the user the his suggested Signs')
 def suggested_signs(context):
     suggested_signs = context.browser.find_element_by_id("signsFactoryMyDiscussions")
-    suggested_signs.text()
     assert_equal("Meus sinais sugeridos", suggested_signs)
     context.browser.quit()
